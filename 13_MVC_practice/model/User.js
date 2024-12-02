c1c0237289eb81caef88a279a26253f2d75a1948
1roo <93hanaru@gmail.com>
2024-12-02 11:54:59 +0900

    241202: 지난 mvc 실습 2문제 풀이

---
+++ 13_MVC_practice/model/User.js
@@ -0,0 +1,12 @@
+// 실습1
+exports.UserInfo = () => {
+  return {
+    realId: "banana",
+    realPw: 4321,
+  };
+};
+
+// 실습2
+exports.user = `apple//1234//사과사과
+banana//4321//바나나나나
+cocoa//qwer1234//미떼`;
