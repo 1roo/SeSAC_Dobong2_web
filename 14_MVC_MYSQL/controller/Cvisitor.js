0b5f5015d60360164960562e107e53ae38f2a6ac
1roo <93hanaru@gmail.com>
2024-12-02 15:38:40 +0900

    241202: MVC와 MUSQL연결하기

---
+++ 14_MVC_MYSQL/controller/Cvisitor.js
@@ -0,0 +1,19 @@
+const Visitor = require("../model/Visitor");
+
+/* '/' GET */
+exports.main = (req, res) => {
+  res.render("index");
+};
+
+/* '/visitors' GET */
+exports.getVisitors = (req, res) => {
+  // console.log(Visitor.getVisitors());
+  // [DB 연결 전]
+  // res.render("visitors", { data: Visitor.getVisitors() });
+
+  // [DB 연결 후]
+  Visitor.getVisitors((result) => {
+    console.log("전체목록 Cvisitor.js", result);
+    res.render("visitors", { data: result });
+  });
+};
