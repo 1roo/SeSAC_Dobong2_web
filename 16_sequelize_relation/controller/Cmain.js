0a1350117f9f362232aa4ae9c548c90e82454645
1roo <93hanaru@gmail.com>
2024-12-09 14:16:37 +0900

    sequelize 모델(테이블)간 관계

--- 16_sequelize_relation/controller/Cmain.js
+++ 16_sequelize_relation/controller/Cmain.js
@@ -1,3 +1,181 @@
+const { Op } = require("sequelize");
+const { Player, Profile, Team } = require("../models");
+
 exports.main = (req, res) => {
   res.render("index");
+};
+
+// GET /players
+exports.getAllPlayers = async (req, res) => {
+  try {
+    // select * from player
+    const players = await Player.findAll();
+    console.log(players);
+    res.send(players);
+  } catch (err) {
+    console.log("err", err);
+    res.status(500).send("server error");
+  }
+};
+
+// GET /players/:playerId
+// player, pforile 정보 조회 >> join 필요
+exports.getPlayer = async (req, res) => {
+  try {
+    // select * from Player where playerId
+    console.log(req.params); //{ playerId: '2' }
+    const { playerId } = req.params;
+    const player = await Player.findOne({
+      where: { player_id: playerId },
+      include: [{ model: Profile, attributes: ["position", "salary"] }],
+      // attributes의 배열은 profil테이블의 컬럼명과 일치해야 함.
+    });
+    res.send(player);
+  } catch (err) {
+    console.log("err", err);
+    res.status(500).send("server error");
+  }
+};
+
+// POST /players (선수 추가)
+/*
+{
+  name: '손흥민',
+  age: 30,
+  team_id: 1
+}
+*/
+exports.postPlayer = async (req, res) => {
+  try {
+    console.log(req.body);
+    //   { name: '손흥민', age: 30, teamid: 1 }
+    //   const newPlayer = await Player.create({
+    //     컬럼명: 넣을 데이터
+    //     name: req.body.name,
+    //     age: req.body.age,
+    //     teamid: req.body.teamid,
+    //     )};
+    res.send("response");
+  } catch (err) {
+    console.log("err", err);
+    res.status(500).send("server error");
+  }
+};
+
+// Patch /players/:playerId/team (선수 소속팀 변경)
+exports.patchPlayer = async (req, res) => {
+  try {
+    // console.log("param: ", req.params);
+    // param:  { playerId: '2' }
+    // console.log("body: ", req.body);
+    // body:  { teamId: 1 }
+
+    const updatedPlayer = await Player.update(
+      {
+        // 어떤 컬럼을 바꿀지
+        teamid: req.body.teamId,
+      },
+      {
+        // where조건 작성
+        where: {
+          player_id: req.params.playerId,
+        },
+      }
+    );
+    res.send(updatedPlayer);
+  } catch (err) {
+    console.log("err", err);
+    res.status(500).send("server error");
+  }
+};
+
+// DELETE /players/:playerId (선수 삭제)
+exports.deletePlayer = async (req, res) => {
+  try {
+    // console.log(req.params);
+    // { playerId: '2' }
+    const { playerId } = req.params;
+    const isDeleted = await Player.destroy({
+      where: {
+        player_id: playerId,
+      },
+    });
+    if (Boolean(isDeleted)) res.send("삭제 성공");
+    else res.send(false);
+  } catch (err) {
+    console.log("err", err);
+    res.status(500).send("server error");
+  }
+};
+
+/* 복잡한 where조건 사용해보기 */
+// GET /teams
+// 정렬, 검색 >> req.query 사용
+exports.getTeams = async (req, res) => {
+  try {
+    console.log(req.query); // {} {sort} {search}
+    const { sort, search } = req.query;
+    let teams;
+    if (sort === "name_asc") {
+      // 팀 이름순으로 정렬해서 조회
+      teams = await Team.findAll({
+        attributes: ["team_id", "name"], // select team_id, name from ~
+        order: [["name", "ASC"]], // order by name ASC
+      });
+    } else if (search) {
+      // search 키워드 기준으로 검색후 조회
+      teams = await Team.findAll({
+        attributes: ["team_id", "name"],
+        where: {
+          name: { [Op.like]: `%${search}%` }, // where name like '%k%'
+        },
+      });
+    } else {
+      // 전체 조회
+      teams = await Team.findAll({
+        attributes: ["team_id", "name"],
+      });
+    }
+    // 검색 로직 종료
+
+    // ----- 응답 -----
+    if (teams.length === 0) res.send("다시 검색하세요");
+    else res.send(teams);
+  } catch (err) {
+    console.log("err", err);
+    res.status(500).send("server error");
+  }
+};
+
+// GET /teams/:teamId
+// 특정 팀 조회
+exports.getTeam = async (req, res) => {
+  try {
+    const { teamId } = req.params;
+    const team = await Team.findOne({
+      where: {
+        team_id: teamId,
+      },
+    });
+    res.send(team);
+  } catch (err) {
+    console.log("err", err);
+    res.status(500).send("server error");
+  }
+};
+
+// GET /teams/:teamId/players
+// 특정 팀의 모든 선수 조회 >> join
+exports.getTeamPlayers = async (req, res) => {
+  try {
+    const { teamId } = req.params;
+    const players = await Team.findOne({
+      where: { team_id: teamId },
+      include: [{ model: Player, attributes: ["player_id", "name", "age"] }],
+    });
+    res.send(players);
+  } catch (err) {
+    console.log("err", err);
+    res.status(500).send("server error");
+  }
 };
--- 16_sequelize_relation/models/index.js
+++ 16_sequelize_relation/models/index.js
@@ -5,7 +5,7 @@ const config = require(__dirname + "/../config/config.js")["dev"];
 const db = {};
 
 // 1. Sequelize 클래스를 통해서 sequelize 인스턴스 생성
-let sequelize = new Sequelize(
+const sequelize = new Sequelize(
   config.database,
   config.username,
   config.password,
@@ -20,6 +20,37 @@ const TeamModel = require("./Team")(sequelize, Sequelize);
 const TeamGameModel = require("./TeamGame")(sequelize, Sequelize);
 
 // 3. 모델간 관계 설정
+// 3-1. Player : Profile = 1:1
+PlayerModel.hasOne(ProfileModel, {
+  onDelete: "CASCADE",
+  onUpdate: "CASCADE",
+  foreignKey: "player_id",
+});
+ProfileModel.belongsTo(PlayerModel, {
+  foreignKey: "player_id", // 내가 정해준 이름
+});
+
+// 3-2. Team : Player = 1:N
+TeamModel.hasMany(PlayerModel, {
+  foreignKey: "teamid", // 내가 정해주는 이름
+  sourceKey: "team_id", // 실제로 참조할 이름
+});
+// 실제 team model의 컬럼명과 일치해야 함.
+// models/Team.js의 PK
+PlayerModel.belongsTo(TeamModel, {
+  foreignKey: "teamid", // 내가 정해주는 이름
+  targetKey: "team_id", // 실제로 참조할 이름
+});
+
+// 3-3. Team : Game = M:N => 중계테이블 Teamgame 활용
+TeamModel.belongsToMany(GameModel, {
+  through: TeamGameModel,
+  foreignKey: "team_id", // 내가 정해주는 이름, for team model
+});
+GameModel.belongsToMany(TeamModel, {
+  through: TeamGameModel,
+  foreignKey: "game_id", // 내가 정해주는 이름
+});
 
 // 4. db객체에 모델 추가
 db.Player = PlayerModel;
