11a1876a73e27ff56a74fec42c2a25c60454ce0b
1roo <93hanaru@gmail.com>
2025-01-15 17:11:58 +0900

    250113 리액트 ref, 생명주기

---
+++ 22_ref-lifecycle/src/components/Test.jsx
@@ -0,0 +1,9 @@
+import "../style/test.css";
+export default function Test() {
+  return (
+    <div className="container">
+      <div className="red"></div>
+      <div className="yellow"></div>
+    </div>
+  );
+}
