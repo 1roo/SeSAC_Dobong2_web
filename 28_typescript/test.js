6eff21ad398fa8a82a8b369c8dd5e154e815e4c7
hanaru <133945249+1roo@users.noreply.github.com>
2025-02-02 15:56:51 +0900

    240131 react ts 연습

--- 28_typescript/test.js
+++ 28_typescript/test.js
@@ -16,3 +16,12 @@ jsPrint(1, 2, 3, 4, 5);
 function addSometing(a, b) {
   return a + b; // number
 }
+
+function sum(...nums) {
+  let sum = 0;
+  for (let num of nums) {
+    sum += num;
+  }
+  return sum;
+}
+console.log(sum(1, 2, 3, 4, 5));
---
+++ 29_react-typescript/src/types/interface.ts
@@ -0,0 +1,11 @@
+export interface TodoItemInterface {
+  id: number;
+  text: string;
+  done: boolean;
+}
+export interface MatzipInterface {
+  idx?: number;
+  imgSrc: string;
+  title: string;
+  desc: string;
+}
