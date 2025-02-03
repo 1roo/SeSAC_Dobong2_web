f94afe9b6edb6cd30a9e28d3a6416812232addeb
1roo <93hanaru@gmail.com>
2025-02-03 15:00:39 +0900

    250203 aws s3 multer 사용용

---
+++ 30_s3/app.js
@@ -0,0 +1,71 @@
+// npm i aws-sdk multer-s3@2.10.0
+
+const express = require("express");
+const dotenv = require("dotenv");
+const multer = require("multer");
+const aws = require("aws-sdk");
+const multerS3 = require("multer-s3");
+
+const path = require("path");
+
+const app = express();
+const PORT = 8080;
+dotenv.config();
+
+// view engine 설정정
+app.set("view engine", "ejs");
+
+// aws s3 설정
+aws.config.update({
+  accessKeyId: process.env.AWS_S3_ACCESS_KEY,
+  secretAccessKey: process.env.AWS_S3_SECRET_ACCESS_KEY,
+  region: process.env.AWS_S3_REGION,
+});
+
+const s3 = new aws.S3();
+
+// multer 설정
+// 서버에 사진을 저장하는 기존 코드
+// const storage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, "uploads/");
+//   },
+//   filename: (req, file, cb) => {
+//     const ext = path.extname(file.originalname); // 확장자자
+//     cb(null, path.basename(file.originalname, ext) + Date.now() + ext);
+//   },
+// });
+
+// s3(클라우드) multer 설정
+const upload = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: process.env.AWS_S3_BUCKET,
+    acl: "public-read", // 파일 접근 권한 설정
+
+    key: (req, file, cb) => {
+      cb(null, Date.now().toString() + "-" + file.originalname);
+    },
+  }),
+});
+
+app.get("/", (req, res) => {
+  res.render("index", { imageUrl: "" });
+});
+
+app.post("/upload", upload.single("image"), (req, res) => {
+  console.log(req.file);
+
+  if (req.file) {
+    const imageUrl = req.file.location; // S3에 업로드된 파일의 경로
+    res.render("index", { imageUrl });
+  } else {
+    res.send("이미지 주세요");
+  }
+
+  res.send("이미지 잘 받았음");
+});
+
+app.listen(PORT, () => {
+  console.log(`http://localhost:${PORT}`);
+});
